34e7e91ad6c5a20c3a9f6f2c6618e383c37e16b3
tonystark0943 <tonystark0943@gmail.com>
2025-10-14 09:13:39 +0530

    Add files via upload

---
+++ lab_ass2.py
@@ -0,0 +1,48 @@
+# Lab Assignment 2
+# Name: Pratik Ranjan Mahala
+# Regd No: 24E113A07
+# Serial No: 15
+
+# #1
+# str = input("Enter a string: ")
+# idx = -len(str)
+# for i in range(len(str)):
+#     print(f"The character present at positive index {i} and negative index {idx} is {str[i]}")
+#     idx+=1
+
+# #2a
+# s = input("Enter a string: ")
+# print(f"Original String: {s}")
+# print(f"Strip: {s.strip("e")}")
+# print(f"Length: {len(s)}")
+# print(f"Right Strip: {s.rstrip("er")}")
+# print(f"Left Strip: {s.lstrip("He")}")
+# print(f"Find: {s.find("l")}")
+# print(f"RFind: {s.rfind("l")}")
+# print(f"Index: {s.index("l")}")
+# print(f"RIndex: {s.rindex("l")}")
+# print(f"Count: {s.count("l")}")
+# print(f"Replace: {s.replace("l", "p")}")
+# print(f"Split: {s.split("e")}")
+# print(f"Join: {"_".join(s)}")
+# print(f"Uppercase: {s.upper()}")
+# print(f"Lowercase: {s.lower()}")
+# print(f"Swapcase: {s.swapcase()}")
+# print(f"Titlecase: {s.title()}")
+# print(f"Capitalize: {s.capitalize()}")
+# print(f"Startswith: {s.startswith("He")}")
+# print(f"Endswith: {s.endswith("lo")}")
+
+# #3
+# str = input("Enter a string: ")
+# i = len(str)-1
+# while i>=0:
+#     print(str[i], end="")
+#     i-=1
+
+# #4
+# s = input("Enter a string: ")
+# for ch in s:
+#     print(chr(ord(ch)+3), end="")
+
+#5 
